e88045eb5c92931911f80ea343571f2add5a8244
David Torroija <davidtorroija@gmail.com>
2019-05-30 12:54:55 -0500

    trying to inject scripts and take the screenshot

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ const puppeteer = require("puppeteer");
         "https://www.w3schools.com/css/css_float.asp"
         , {waitUntil: 'networkidle0'}
     );
-    await page.waitForSelector(".w3-main");
     await page.addScriptTag({url: 'https://html2canvas.hertzen.com/dist/html2canvas.js'});
     await page.addScriptTag({url: 'https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js'});
     await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
+    await page.waitForSelector(".w3-main");
+    // await page.waitForFunction('document.querySelectorAll(".w3-main").length > 0');
+    // await page.waitFor(5000)
     const screenshot = await page.evaluate(async () => {
-        const canvasElement = await html2canvas($(".w3-main")[0], {
-            windowWidth: 1100,
-            useCORS: true,
+        const canvasElement = await html2canvas($("div")[0], {
+            // useCORS: true,
         });
 
         let image = Canvas2Image.convertToImage(
